6edfeb5bb651311f156784749a2a1238c1811de1
ByF3li <fehaider@tsn.at>
2022-11-03 10:04:19 +0100

    sumFunction

---
+++ Algorythm/src/sumFunc.java
@@ -0,0 +1,35 @@
+public class sumFunc {
+
+    public static void main(String[] args) {
+        int zahl = 6;
+
+        System.out.print("Rekursiv" + sumFunctionRecursive(zahl));
+
+        System.out.println("Iterativ" + sumFunctionIterative(zahl));
+
+
+
+
+    }
+
+    public static int sumFunctionRecursive(int number){
+
+        if(number == 1){
+            return number = 1;
+        }
+        else{
+            return number = number + sumFunctionRecursive(number = number - 1);
+        }
+    }
+
+    public static int sumFunctionIterative(int number){
+        int num = 1;
+        while(number > 1){
+            num = num + number;
+            number--;
+        }
+        return num;
+    }
+
+
+}
